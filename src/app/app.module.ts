e353856d4246bc73bfbd96adb0ea60112dee7309
Haixiang <haixiang6123@gmail.com>
2019-04-01 16:20:29 -0700

    Add all components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SettingComponent } from './pages/setting/setting.component';
+import { VotesComponent } from './pages/votes/votes.component';
+import { FavouritesComponent } from './pages/favourites/favourites.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AnalysisComponent } from './components/analysis/analysis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingComponent,
+    VotesComponent,
+    FavouritesComponent,
+    RegisterComponent,
+    HeaderComponent,
+    AnalysisComponent
   ],
   imports: [
     BrowserModule,
