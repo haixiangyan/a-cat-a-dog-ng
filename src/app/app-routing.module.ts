68fb73d34b1f6008e3bf9198c0ba885ae7d98793
Haixiang <haixiang6123@gmail.com>
2019-04-01 16:23:06 -0700

    Config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {SettingComponent} from './pages/setting/setting.component';
+import {VotesComponent} from './pages/votes/votes.component';
+import {FavouritesComponent} from './pages/favourites/favourites.component';
+import {RegisterComponent} from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'setting', component: SettingComponent},
+  {path: 'votes', component: VotesComponent},
+  {path: 'favourites', component: FavouritesComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
